598a72489950a608d413067af52b07f43df8ad97
Yang Liu <jeremyrobturtle@gmail.com>
2016-11-15 16:10:51 -0500

    tweak example code

--- include/storage/test.cpp
+++ include/storage/test.cpp
@@ -8,6 +8,6 @@ using namespace mips::storage;
 int main(void) {
   Storage<8, 1024, BigEndian> imem = {1, 2, 3, 4, 5, 6, 7, 8};
   cout << imem.read<32>(0) << endl;
-  cout << imem.read<32>(4) << endl;
-  cout << imem.read<32>(8) << endl;
+  cout << imem.read<16>(4) << endl;
+  cout << imem.read<16>(6) << endl;
 }
