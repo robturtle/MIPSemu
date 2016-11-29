ed94f862af55cae865b44f5776f517e18f65cb52
Yang Liu <jeremyrobturtle@gmail.com>
2016-11-28 19:10:37 -0500

    only print out full way groups

--- lab2/cache_debug.h
+++ lab2/cache_debug.h
@@ -9,6 +9,11 @@
 template <typename WayInfo>
 void inspect_way_tags(std::vector<WayInfo> infos, size_t ways_per_index)
 {
+  if (ways_per_index == 1)
+  {
+    std::cout << "It's a directed mapped cache. Skipped.\n";
+    return;
+  }
   auto it = infos.begin(), end = infos.end();
   size_t index = 0;
   while (it != end)
@@ -16,17 +21,17 @@ void inspect_way_tags(std::vector<WayInfo> infos, size_t ways_per_index)
     std::ostringstream group_tags;
     group_tags << std::hex;
     group_tags << "index " << index << ": ";
-    bool not_empty = false;
+    bool full = true;
     for (size_t i = 0; i < ways_per_index; i++, ++it)
     {
-      if ((*it).valid)
+      if (!(*it).valid)
       {
-        not_empty = true;
+        full = false;
       }
       if (i != 0) group_tags << " | ";
       group_tags << std::setw(10) << (*it).tag;
     }
-    if (not_empty) std::cout << group_tags.str() << '\n';
+    if (full) std::cout << group_tags.str() << '\n';
     index++;
   }
 }
